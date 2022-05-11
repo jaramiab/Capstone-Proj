7ad1db23e3c2d40f0b7831079bc7498be85255a4
J-Rod239 <ProJose239@outlook.com>
2022-05-11 11:38:22 -0400

    Text placement

--- market.py
+++ market.py
@@ -22,10 +22,9 @@ my_canvas.configure(yscrollcommand=scroll.set)
 my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion= my_canvas.bbox("all")))
 
 secondFrame = Frame(my_canvas)
-my_canvas.create_window((0,0), window=secondFrame, anchor='nw')
-price = Button(secondFrame, text='Price', command=None, borderwidth=0)
-price.pack(anchor=NE, side=RIGHT)
+my_canvas.create_window((0,0), window=secondFrame)
 
+#Grey Blocks
 my_canvas.create_rectangle(10, 120, 500, 10, fill='grey')
 my_canvas.create_rectangle(10, 250, 500, 125, fill='grey')
 my_canvas.create_rectangle(10, 380, 500, 255, fill='grey')
@@ -33,8 +32,41 @@ my_canvas.create_rectangle(10, 500, 500, 385, fill='grey')
 my_canvas.create_rectangle(10, 505, 500, 645, fill='grey')
 my_canvas.create_rectangle(10, 650, 500, 780, fill='grey')
 my_canvas.create_rectangle(10, 790, 500, 880, fill='grey')
+
+#First Block Text
 my_canvas.create_text(50, 20, fill='black', font='Arial', text='Item Name')
 my_canvas.create_text(75, 65, fill='black', font='Arial 15', text='Item Description')
+price = Button(my_canvas, text='Price', command=None, borderwidth=0)
+price.pack(padx=35, pady=25, anchor=E)
 
+#Second Block Text
+my_canvas.create_text(50, 135, fill='black', font='Arial', text='Item Name')
+my_canvas.create_text(75, 180, fill='black', font='Arial 15', text='Item Description')
+price2 = Button(my_canvas, text='Price', command=None, borderwidth=0)
+price2.pack(padx=35, pady=125, anchor=E)
+
+#Third Block Text
+my_canvas.create_text(50, 265, fill='blue', font='Arial', text='Item Name')
+my_canvas.create_text(75, 300, fill='blue', font='Arial 15', text='Item Description')
+price3 = Button(my_canvas, text='Price', command=None, borderwidth=0)
+price3.pack(padx=35, pady=275, anchor=E)
+
+#Fourth Block Text
+my_canvas.create_text(50, 400, fill='red', font='Arial', text='Item Name')
+my_canvas.create_text(75, 435, fill='red', font='Arial 15', text='Item Description')
+price4 = Button(my_canvas, text='Price', command=None, borderwidth=0)
+price4.pack(padx=35, pady=135, anchor=E)
+
+#Fifth Block Text
+my_canvas.create_text(50, 665, fill='black', font='Arial', text='Item Name')
+my_canvas.create_text(75, 700, fill='black', font='Arial 15', text='Item Description')
+price5 = Button(my_canvas, text='Price', command=None, borderwidth=0)
+price5.pack(padx=35, pady=650, anchor=E)
+
+#Sixth Block Text
+my_canvas.create_text(50, 520, fill='green', font='Arial', text='Item Name')
+my_canvas.create_text(75, 570, fill='green', font='Arial 15', text='Item Description')
+price6 = Button(my_canvas, text='Price', command=None, borderwidth=0)
+price6.pack(padx=35, pady=790, anchor=E)
 
 root.mainloop()
